f186c0131ed47cb40c161ca5ad17715dd5ceb71d
Samuel Costa <132624144+samuelbarsanellicosta@users.noreply.github.com>
2024-12-09 12:07:44 -0300

    tailored for pop os fresh install

--- install.sh
+++ install.sh
@@ -43,17 +43,7 @@ fi
 cd "$CURRENT_DIR"
 
 # Symlink VS Code settings and keybindings to the present `settings.json` and `keybindings.json` files
-# If it's a macOS
-if [[ `uname` =~ "Darwin" ]]; then
-  CODE_PATH=~/Library/Application\ Support/Code/User
-# Else, it's a Linux
-else
-  CODE_PATH=~/.config/Code/User
-  # If this folder doesn't exist, it's a WSL
-  if [ ! -e $CODE_PATH ]; then
-    CODE_PATH=~/.vscode-server/data/Machine
-  fi
-fi
+CODE_PATH=~/.config/Code/User
 
 for name in settings.json keybindings.json; do
   target="$CODE_PATH/$name"
@@ -61,15 +51,7 @@ for name in settings.json keybindings.json; do
   symlink $PWD/$name $target
 done
 
-# Symlink SSH config file to the present `config` file for macOS and add SSH passphrase to the keychain
-if [[ `uname` =~ "Darwin" ]]; then
-  target=~/.ssh/config
-  backup $target
-  symlink $PWD/config $target
-  ssh-add --apple-use-keychain ~/.ssh/id_ed25519
-fi
-
 # Refresh the current terminal with the newly installed configuration
 exec zsh
 
-echo "👌 Carry on with git setup!"
+echo "Setup completed!"
